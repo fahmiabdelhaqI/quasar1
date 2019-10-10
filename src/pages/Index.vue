4bbfa58244d402da02aa1577f6665be54b0f7770
fahmiabdelhaqI <fahmibaru01@gmail.com>
2019-10-10 18:13:11 +1100

    tambah uppercase

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,7 +5,10 @@
      @keyup= "handleKeyup"
      />
     <button @click="clearMessage"> clear </button>
-    <h5> {{message}}</h5>
+    <h5 v-if="message.length"> {{message}}</h5>
+    <h6 v-else>No massage</h6>
+
+    <p>{{uppercaseMessage()}}</p>
   </q-page>
 </template>
 
@@ -28,6 +31,9 @@ export default {
       if(e.keyCode == 27 ){
         this.message = 'ESC'
       }
+    },
+   uppercaseMessage() {
+      return this.message.toUpperCase()
     }
   }
 }
